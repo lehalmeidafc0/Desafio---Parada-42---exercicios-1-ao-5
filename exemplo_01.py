20eb5d4d7aaa9046ff8ec20fd86dff7f74a44162
lehalmeidafc0 <144854855+lehalmeidafc0@users.noreply.github.com>
2024-08-05 19:00:23 -0300

    Update exemplo_01.py

---
+++ exemplo_01.py
@@ -0,0 +1,11 @@
+# Solicita ao usuário que digite o primeiro valor
+valor1 = float(input("Digite o primeiro valor: "))
+
+# Solicita ao usuário que digite o segundo valor
+valor2 = float(input("Digite o segundo valor: "))
+
+# Calcula a soma dos dois valores
+soma = valor1 + valor2
+
+# Exibe o resultado da soma
+print("A soma dos valores é:", soma)
